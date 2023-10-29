b47231a1bcf2a1f84ddba8745db85b8b32e971d0
veetir <66322733+veetir@users.noreply.github.com>
2023-10-30 00:51:03 +0200

    Refactored code structure and added program argument input
    - Split the code into separate Python files for better organization:
        - `data_loading.py` for data loading functions.
        - `model_training.py` for training the model and evaluating metrics + Dummy classifier
        - `visualization.py` for functions related to data visualization.
        - `risk.py` contains the main method for user interaction.
    - Updated the program to accept user input for program arguments.

---
+++ risk.py
@@ -0,0 +1,38 @@
+from data_loading import give_stock_data, give_event_data
+from model_training import train_first_model
+from visualization import plot_stock_price, calculate_event_frequency
+
+def main():
+    default = input("Run with default arguments? (y/n): ").lower()
+    while default not in ['y', 'n']:
+        default = input("Please enter 'y' or 'n': ").lower()
+
+    if default == "y":
+        stock_name = "aapl"
+        event_t = 1
+        n_splits = 20
+        n_estimators = 150
+    else:
+        stock_name = input("Enter the stock ID (e.g., aapl): ")
+        event_t = -float(input("Enter the event threshold (e.g., 1 for 1% dip): "))
+        n_splits = int(input("Enter the number of splits for time series cross-validation: "))
+        n_estimators = int(input("Enter the number of estimators for the Random Forest model: "))
+
+    # Load stock data based on user input
+    stock_data = give_stock_data(stock=stock_name)
+
+    # Plot stock prices
+    plot_stock_price(stock_data, stock_name=stock_name)
+
+    # Calculate event data based on a threshold
+    stock_data = give_event_data(stock_data, event_threshold=event_t)
+
+    # Calculate event frequency
+    calculate_event_frequency(stock_data, event_threshold=event_t)
+
+    # Train models
+    train_first_model(stock_data, n_splits=n_splits, n_estimators=n_estimators)
+
+if __name__ == "__main__":
+    main()
+
