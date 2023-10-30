859f12ba544e0074a5bf986ee593d3baf0406cb3
veetir <66322733+veetir@users.noreply.github.com>
2023-10-30 14:07:18 +0200

    Training & testing sets. Plot test set instead of training set

--- risk.py
+++ risk.py
@@ -1,6 +1,6 @@
 from data_loading import give_stock_data, give_event_data
 from model_training import train_first_model
-from visualization import plot_stock_price, calculate_event_frequency
+from visualization import plot_stock_price, plot_stock_price_with_events, calculate_event_frequency
 
 def main():
     default = input("Run with default arguments? (y/n): ").lower()
@@ -19,19 +19,20 @@ def main():
         n_estimators = int(input("Enter the number of estimators for the Random Forest model: "))
 
     # Load stock data based on user input
-    stock_data = give_stock_data(stock=stock_name)
-
-    # Plot stock prices
-    plot_stock_price(stock_data, stock_name=stock_name)
+    train_data, test_data = give_stock_data(stock=stock_name)
 
     # Calculate event data based on a threshold
-    stock_data = give_event_data(stock_data, event_threshold=event_t)
+    train_data = give_event_data(train_data, event_threshold=event_t)
+    test_data = give_event_data(test_data, event_threshold=event_t)
 
     # Calculate event frequency
-    calculate_event_frequency(stock_data, event_threshold=event_t)
+    calculate_event_frequency(train_data, event_threshold=event_t)
+
+    # Plot stock prices
+    plot_stock_price_with_events(test_data, stock_name=stock_name)
 
     # Train models
-    train_first_model(stock_data, n_splits=n_splits, n_estimators=n_estimators)
+    train_first_model(train_data, n_splits=n_splits, n_estimators=n_estimators)
 
 if __name__ == "__main__":
     main()
