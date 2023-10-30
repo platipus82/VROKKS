fbdd2bebe68207373d857097854e07f0878a7905
platipus82 <39970379+platipus82@users.noreply.github.com>
2023-10-30 12:18:05 +0200

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -11,6 +11,30 @@ def plot_stock_price(stock_data, stock_name="aapl"):
     plt.tight_layout()
     plt.show()
 
+def plot_stock_price_with_events(stock_data, stock_name="aapl"):
+    # Plotting the Close prices over time
+    plt.figure(figsize=(10, 6))
+
+    # Create a boolean mask for events
+    event_mask = stock_data['event']
+
+    # Plot the Close prices in blue
+    plt.plot(stock_data['Date'], stock_data['Close'], color='blue', label='Close Price')
+
+    # Highlight events by coloring them in red
+    plt.plot(stock_data['Date'][event_mask], stock_data['Close'][event_mask], color='red', marker='o', linestyle='', label='Event')
+
+    plt.title('Stock ' + stock_name + ': Close Prices Over Time')
+    plt.xlabel('Date')
+    plt.ylabel('Close Price')
+    plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
+    plt.tight_layout()
+
+    # Add a legend to distinguish between Close Price and Events
+    plt.legend()
+
+    plt.show()
+
 def calculate_event_frequency(stock_data, event_threshold):
     # Calculate frequency of event1 (1% dip) and event2 (10% dip)
     event_count = stock_data['event'].sum()
